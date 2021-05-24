f69c8da592371cfb16ddd53988aa89cc5ced6c08
kate123wong <18810809501@163.com>
2021-05-24 21:27:07 +0800

    add notes of webpack

--- webpack/01Webpack/webpack.config.js
+++ webpack/01Webpack/webpack.config.js
@@ -22,7 +22,29 @@ module.exports = {
         rules:[
             //详细的loader配置
             { test: /\.css$/, use: ['style-loader','css-loader']},
-            { test: /\.ts$/, use: 'ts-loader' }
+            { test: /\.ts$/, use: 'ts-loader' },
+            { 
+                // 问题：默认处理不了html中的img图片
+                test: /\.jpg$/,
+                // url-loader下载url-loader file-loader
+                //使用一个loader用loader，使用多个loader用use
+                loader: 'url-loader',
+                options: {
+                    //图片大小小于8kb，就会被base64处理
+                    // 优点：减少请求数量，减轻服务器压力
+                    // 缺点：图片体积会更大，文件请求速度慢
+                    limit: 8 * 1024,
+                    esModule: false,
+                    //[hash:10]是取图片hash的前十位，
+                    // [ext]取文件原来的扩展名
+                    name: '[hash:10].[ext]'
+                }
+            },
+            { 
+                test: /\.html$/,
+                //处理html文件中的img图片（负责引入img，从而能被url-loader处理
+                loader: 'html-loader'
+            }
         ]
     },
     plugins:[
